5ab9ffbcabd978ebb2321c2a2054c62a8ed2172d
Kip West <jacobkwest93@gmail.com>
2021-09-18 16:03:20 -0600

    add ApolloClient

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+/**
+ApolloClient is the class that represents the Apollo Client itself
+  @param uri specifies the location of our GraphQL server.
+  @param cache every instance of ApolloClient uses in-memory cache; this enables it to store and reuse query results so ti doesn't have to make as many network requests (aka faster/more efficient)
+*/
+const client = new ApolloClient({
+  // options go here
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
